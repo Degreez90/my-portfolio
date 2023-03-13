015645da6fd98998f7b2e80200e6ffc7322b3223
Offitt <offitt90@gmail.com>
2023-03-13 02:31:08 -0400

    fixed responsiveness on smaller screens

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
     >
       <div className='relative w-full h-full bg-slate-800/[.40]'>
         <Nav />
-        <div className='mx-auto container relative bottom-12 md:bottom-0 grid grid-rows-2 items-center justify-items-center md:justify-items-center md:grid-rows-none md:grid-cols-2 gap-3 px-5'>
+        <div className='mx-auto container relative bottom-12 md:bottom-0 grid grid-rows-2 items-center justify-items-center md:justify-items-center md:grid-rows-none md:grid-cols-2 gap-3'>
           <div className='pt-10 md:pt-32 md:max-w-[365px] text-center font-[raleway] text-4xl text-white'>
             <h1 className='text-5xl'>
               Web<span className='text-emerald-500'>Me</span>
