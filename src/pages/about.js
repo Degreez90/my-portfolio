afd51168bc756a582dd13c04797823647f249780
Offitt <offitt90@gmail.com>
2023-03-15 04:07:16 -0400

    n/a

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Documentation from '../components/Documentation';
-import Footer from '../components/footer';
 import HeroB from '../components/HeroB';
 import Layout from '../components/Layout';
 
